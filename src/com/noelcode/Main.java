5990913adc95b1dfd764a9b658e9077074c8471c
Noel Guillen <noelblas274@gmail.com>
2024-04-05 20:44:43 -0400

    making exception class for methods

--- src/com/noelcode/Main.java
+++ src/com/noelcode/Main.java
@@ -1,24 +1,19 @@
 package com.noelcode;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-//        String name = "noel";
-//        System.out.println( name.indexOf("f"));
-//        System.out.println(" ".isBlank());
-//        System.out.println(name.charAt(0));
-//        System.out.println(name.charAt(0));
-//        String[] names = {"noel", "hello"};
-//
-//        System.out.println(String.join(" || ",names).trim());
-        Scanner scan = new Scanner(System.in);
-        String name = scan.nextLine();
-        System.out.println(name);
-        scan.close();
+        try {
+            System.out.println(divide(10, 0));
+        } catch (MyCheckedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public static int divide(int a, int b) throws MyCheckedException{
+        if(b == 0){
+            throw new MyCheckedException("Sorry int needs to be greater then 1");
+        }
+        return a / b;
     }
 
 }
---
+++ src/com/noelcode/MyUnCheckedException.java
@@ -0,0 +1,9 @@
+package com.noelcode;
+
+public class MyUnCheckedException extends Exception{
+
+    public MyUnCheckedException(String message) {
+
+        super(message);
+    }
+}
